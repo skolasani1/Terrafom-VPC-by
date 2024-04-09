9c4f63f3d330a7579a29a11dd17a16e2f210b30f
skolasani1 <126361111+skolasani1@users.noreply.github.com>
2024-04-08 22:21:45 -0700

    Update variable.tf

--- Terraform-VPC/modules/EC2/variable.tf
+++ Terraform-VPC/modules/EC2/variable.tf
@@ -8,7 +8,7 @@ variable "subnets" {
     type = list(string)
 }
 
-variable "ec2_names" {
+variable "EC2_names" {
     description = "EC2 names"
     type = list(string)
     default = ["webserver1", "appserver2"]
