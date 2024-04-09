fd7e7328bd822b1b3f83f02867610911bf7d207f
skolasani1 <126361111+skolasani1@users.noreply.github.com>
2024-04-08 21:41:28 -0700

    Update main.tf

--- Terraform-VPC/main.tf
+++ Terraform-VPC/main.tf
@@ -9,8 +9,8 @@ module "sg" {
     vpc_id = module.vpc.vpc_id
 }
 
-module "ec2" {
-    source = "./module/ec2"
+module "EC2" {
+    source = "./modules/ec2"
     sg_id = module.sg.sg_id
     subnets = module.vpc.subnet_ids
 }
